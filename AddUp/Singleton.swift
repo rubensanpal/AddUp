e3650572f0ff9e654f741f2bcd57b7e7f64753c9
Mario Geneau <mario.geneau@gmail.com>
2016-08-29 15:02:49 -0400

    v2

---
+++ AddUp/Singleton.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class Singleton
+{
+    var totalAmountAsFloat: Float = 0.00
+    var totalAmount: String = "$0.00"
+    static let sharedInstance = Singleton()
+}
+
+
+
--- AddUp/ViewController.swift
+++ AddUp/ViewController.swift
@@ -1,25 +1,100 @@
-//
-//  ViewController.swift
-//  AddUp
-//
-//  Created by Mario Geneau on 2016-08-24.
-//  Copyright © 2016 Mario Geneau. All rights reserved.
-//
-
+//=================================
 import UIKit
-
+//=================================
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelNumberToDisplay: UILabel!
+    @IBOutlet weak var plusButton: UIButton!
+    
+    var numberToDisplay: String = ""
+    var decimalClicked: Bool = false
+    var decimalCounter: Int = -1
+    var totalAmount: Float = 0.00
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.labelNumberToDisplay.text = Singleton.sharedInstance.totalAmount
+        self.totalAmount = Singleton.sharedInstance.totalAmountAsFloat
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func buttonsManager(_ sender: UIButton) {
+        switch sender.tag {
+            case 10 :
+                if !decimalClicked {
+                    decimalClicked = true
+                    displayAmount(theString: ".")
+                }
+            case 11 :
+                if sender.alpha != 0.5 {
+                    addingTotal()
+                    sender.alpha = 0.5
+                }
+            default:
+                self.plusButton.alpha = 1.0
+                displayAmount(theString: String(sender.tag))
+        }
+    }
+    
+    private func displayAmount(theString: String) {
+        if decimalClicked {
+            decimalCounter += 1
+            if decimalCounter <= 2 {
+                numberToDisplay += theString
+                labelNumberToDisplay.text = "$\(numberToDisplay)"
+            }
+        } else {
+            numberToDisplay += theString
+            labelNumberToDisplay.text = "$\(numberToDisplay)"
+        }
+    }
+    
+    @IBAction func erase(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Message", message: "Do you really want to erase everything?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) {
+            UIAlertAction in
+                self.numberToDisplay = ""
+                self.labelNumberToDisplay.text = "$0.00"
+                self.decimalClicked = false
+                self.decimalCounter = -1
+                self.totalAmount = 0.00
+                self.plusButton.alpha = 0.5
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) {
+            UIAlertAction in
+            NSLog("Cancel Pressed")
+        }
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    private func addingTotal() {
+        self.totalAmount += Float(numberToDisplay)!
+        self.numberToDisplay = ""
+        self.labelNumberToDisplay.text = String(format: "Total = $%.2f", self.totalAmount)
+        self.decimalClicked = false
+        self.decimalCounter = -1
+    }
+    
+    @IBAction func sendToSingleton(_ sender: UIButton) {
+        Singleton.sharedInstance.totalAmountAsFloat = self.totalAmount
+        Singleton.sharedInstance.totalAmount = String(format: "Total = $%.2f", self.totalAmount)
+    }
 }
+//=================================
+
+
+
+
+
+
+
+
+
+
+
 
